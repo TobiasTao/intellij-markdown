d3f5e23a67c9db9fad3e4255491bed2cc0539143
GitHub Action <action@github.com>
2020-10-13 02:43:42 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/tobiastao/intellijmarkdown/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.tobiastao.intellijmarkdown.services
+
+import com.github.tobiastao.intellijmarkdown.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
